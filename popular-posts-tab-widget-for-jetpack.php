86cc9fb65808ab460632034620379fdadc64f79a
Ryann Micua <ryannmicua@pogidude.com>
2013-03-13 17:13:43 +0800

    added widget title option

--- popular-posts-tab-widget-for-jetpack.php
+++ popular-posts-tab-widget-for-jetpack.php
@@ -119,6 +119,7 @@ class Popular_Posts_Tabbed_Widget_Jetpack extends WP_Widget {
 
 		$instance = $old_instance;
 		
+		$instance['title'] = esc_attr( $new_instance['title'] );
 		$instance['number'] = intval( $new_instance['number'] );
 		$instance['thumb_size'] = intval( $new_instance['thumb_size'] );
 		//$instance['days'] = intval( $new_instance['days'] );
@@ -160,7 +161,12 @@ class Popular_Posts_Tabbed_Widget_Jetpack extends WP_Widget {
    function form( $instance ) { 
 		
 		$instance = wp_parse_args( (array) $instance, $this->defaults );
+		$title = isset( $instance['title'] ) ?esc_attr($instance['title']) : '';
 	?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', $textdomain); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('title'); ?>"  value="<?php echo $title; ?>" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" />
+		</p>
 		<p>
 		   <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts:', PPTWJ_DOMAIN ); ?>
 		   <input class="widefat" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo isset( $instance['number'] ) ? $instance['number'] : ''; ?>" />
@@ -255,9 +261,18 @@ class Popular_Posts_Tabbed_Widget_Jetpack extends WP_Widget {
 			'numberposts' => $number,
 			'thumb' => $thumb_size
 		);
+
+		$title = $instance['title'];
 		?>
 	
 			<?php echo $before_widget; ?>
+
+			<?php
+			/* If a title was input by the user, display it. */
+			if ( !empty( $title ) ){ 
+				echo $before_title . apply_filters( 'widget_title', $title, $instance, $this->id_base ) . $after_title;
+			} ?>
+
 			<div class="pptwj-tabs-wrap">
 	
 				<ul class="tab-links">
